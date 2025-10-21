a24aadb5cf5827bfeae0143f866c2dd843c44196
KentaIshikawa <k.ishikawa0914@gmail.com>
2025-10-21 13:50:48 +0900

    25

--- functions.php
+++ functions.php
@@ -22,3 +22,10 @@ function my_document_title_separator($separator){
 アイキャッチ画像を使用可能にする
 =================*/
 add_theme_support('post-thumbnails');
+
+/*=================
+カスタムメニュー機能を使用可能にする
+=================*/
+add_theme_support('menus');
+
+
