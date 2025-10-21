a8ece379cf1289ff949d1d94d37022f6b2171c70
KentaIshikawa <k.ishikawa0914@gmail.com>
2025-10-21 10:53:24 +0900

    1

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+/*=================
+<title>タグを出力する
+=================*/
+add_theme_support('title-tag');
+
+/*=================
+JSとCSSで、type属性を出力しない様にする
+=================*/
+add_theme_support('html5',['script','style']);
+
+/*=================
+<title>の区切り文字を変更する
+=================*/
+add_filter('document_title_separator','my_document_title_separator');
+function my_document_title_separator($separator){
+    $separator = '|';
+    return $separator;
+}
+
+/*=================
+アイキャッチ画像を使用可能にする
+=================*/
+add_theme_support('post-thumbnails');
