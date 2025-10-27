a2d6401f8f69425edf7804690c3751e60526000d
KentaIshikawa <k.ishikawa0914@gmail.com>
2025-10-27 13:02:50 +0900

    62

---
+++ template-parts/loop-food.php
@@ -0,0 +1,12 @@
+<div class="foodCard">
+    <a href="#">
+        <span class="foodCard_label">オススメ</span>
+        <div class="foodCard_pic">
+            <img src="assets/img/food/food_img01@2x.png" alt="">
+        </div>
+        <div class="foodCard_body">
+            <h4 class="foodCard_title">タコス</h4>
+            <p class="foodCard_price">¥650</p>
+        </div>
+    </a>
+</div>
