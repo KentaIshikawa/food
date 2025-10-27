25ae4feb6734ab368765e72e2365ef429933ae49
KentaIshikawa <k.ishikawa0914@gmail.com>
2025-10-27 10:14:54 +0900

    59

--- functions.php
+++ functions.php
@@ -7,13 +7,14 @@ add_theme_support('title-tag');
 /*=================
 JSとCSSで、type属性を出力しない様にする
 =================*/
-add_theme_support('html5',['script','style']);
+add_theme_support('html5', ['script', 'style']);
 
 /*=================
 <title>の区切り文字を変更する
 =================*/
-add_filter('document_title_separator','my_document_title_separator');
-function my_document_title_separator($separator){
+add_filter('document_title_separator', 'my_document_title_separator');
+function my_document_title_separator($separator)
+{
     $separator = '|';
     return $separator;
 }
@@ -32,7 +33,26 @@ add_theme_support('menus');
 /*=================
 Contact Form 7 の時には整形機能をOFFにする
 =================*/
-add_filter('wpcf7_autop_or_not','my_wpcf7_autop');
-function my_wpcf7_autop(){
+add_filter('wpcf7_autop_or_not', 'my_wpcf7_autop');
+function my_wpcf7_autop()
+{
     return false;
+}
+
+
+/*=================
+メインクエリを変更する
+=================*/
+add_action('pre_get_posts', 'my_pre_get_posts');
+function my_pre_get_posts($query)
+{
+    //管理画面、メインクエリ以外には設定しない
+    if (is_admin() or !$query->is_main_query()) {
+        return;
+    }
+    //トップページの時
+    if ($query->is_home()) {
+        $query->set('posts_per_page', 3);
+        return;
+    }
 }
